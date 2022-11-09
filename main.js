16676071d4a598ef2e0fd854ad6dbb42a6b0dcbd
Evelina Nyman <ina.nyman98@gmail.com>
2022-11-09 15:47:30 +0100

    la till lite JS

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function darkMode() {
-    const element = document.body;
-    element.classList.toggle("darkMode");
+// function darkMode() {
+//     const element = document.body;
+//     element.classList.toggle("darkMode");
+//   }
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
   }
